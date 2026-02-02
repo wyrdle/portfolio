36a95b116778314845ee9cc087a3376cf5de34cc
wyrdle <wyrdle.art@gmail.com>
2026-02-02 15:18:43 +0800

    added tge images and fixed preview

--- scripts/main.js
+++ scripts/main.js
@@ -9,9 +9,8 @@ span.onclick = function() {
 }
 
 
-var images = document.getElementsByClassName('illustration-gallery-item');
+var images = document.querySelectorAll(".illustration-gallery-item, .project-image");
 var modalImg = document.getElementById("img01");
-var captionText = document.getElementById("caption");
 var i;
 for (i = 0; i < images.length; i++) {
   images[i].onclick = function(){
@@ -20,6 +19,12 @@ for (i = 0; i < images.length; i++) {
     modalImg.alt = this.alt;
     captionText.innerHTML = this.nextElementSibling.innerHTML;
   }
+}
+
+var modaltest = document.getElementsByClassName('modal')[0];
+
+modaltest.onclick = function() { 
+    modal.style.display = "none";
 }
 
 var modaltest = document.getElementsByClassName('modal')[0];
